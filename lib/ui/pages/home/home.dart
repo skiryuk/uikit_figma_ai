acfaa1637f3e23a8559ae7116e2813e40374534e
Oleg Skiryuk <oskiryuk@beeline.ru>
2026-01-06 22:43:17 +0300

    Добавлена нижняя таб навигация

--- lib/ui/pages/home/home.dart
+++ lib/ui/pages/home/home.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/cupertino.dart';
+import '../typography/typography.dart';
+import '../palette/palette.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoPageScaffold(child: SafeArea(child: Text('Home Page')));
+    return CupertinoTabScaffold(
+      tabBar: CupertinoTabBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.textformat),
+            label: 'Typography',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.paintbrush),
+            label: 'Palette',
+          ),
+        ],
+      ),
+      tabBuilder: (context, index) {
+        switch (index) {
+          case 0:
+            return const TypographyPage();
+          case 1:
+            return const PalettePage();
+          default:
+            return const TypographyPage();
+        }
+      },
+    );
   }
 }
